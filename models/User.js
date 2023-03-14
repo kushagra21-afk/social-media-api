ff0d4e37c793c64f12773813140698c3ea9794e0
Kushagra <singhalkushagra21@gmail.com>
2023-03-14 09:35:27 +0530

    updating files

--- models/User.js
+++ models/User.js
@@ -20,18 +20,6 @@ const UserSchema = new mongoose.Schema(
       required: true,
       min: 6,
     },
-    profilePicture: {
-      type: String,
-      default: "",
-    },
-    followers: {
-      type: Array,
-      default: [],
-    },
-    followings: {
-      type: Array,
-      default: [],
-    },
     city: {
       type: String,
       max: 50,
